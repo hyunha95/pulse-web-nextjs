9bbedf2d73daeaab832a36dbac703e59ccbca34f
hyunha95 <nohyunha95@gmail.com>
2024-08-11 05:38:24 +0000

    route group 바로 하위에 page.tsx 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import HomePage from "./(nav)/page";
+import HomePage from "./(nav)/HomePage";
 import NavLayout from "./(nav)/layout";
 
 export default function RootPage() {
